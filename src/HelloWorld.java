9dcd79dfc6d8819382e4234cb9fa3d83e7145ca6
Hanbyeol <starhb0412@gmail.com>
2023-02-16 12:53:58 +0900

    [feat] adder 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
+import java.util.function.Function;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        Function<Integer, Integer> adder = (Integer x) -> {
+            return x + 10;
+        };
+
+        int result = adder.apply(5);
+
+        System.out.println(result);
     }
 }
