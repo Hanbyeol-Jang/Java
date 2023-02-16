df2b59db7b568ea1d7dea9ae02dcab90ab4f836f
Hanbyeol <starhb0412@gmail.com>
2023-02-16 12:54:58 +0900

    [fix] 생략 가능

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,7 @@ import java.util.function.Function;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Function<Integer, Integer> adder = (Integer x) -> {
-            return x + 10;
-        };
+        Function<Integer, Integer> adder = x -> x + 10;
 
         int result = adder.apply(5);
 
